fe5884280c1fcac3794bf0e86135b21672c42802
NIRANJAN555 <108479269+NIRANJAN555@users.noreply.github.com>
2023-11-15 12:01:10 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,20 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.neural_network import MLPRegressor
+import pandas as pd
+import joblib
+import os
+df = pd.read_csv('California_Houses.csv')
+df.head()
+df.dropna(axis=0, inplace=True)
+X = df.iloc[:, 1:7]
+y = df.iloc[:, 0]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+model = MLPRegressor()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Training complete!")
+joblib.dump(model, "model.joblib")
+with open("metrics.txt", 'w') as fw:
+ fw.write(f"Mean Squared Error of current model is: {mean_squared_error(y_test, 
+y_pred)}")
